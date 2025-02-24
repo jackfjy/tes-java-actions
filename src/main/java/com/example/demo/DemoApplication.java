8624b91b4d7fdce1e407e8812fe1ade009f7dc60
jackfjy <46479080+jackfjy@users.noreply.github.com>
2025-02-24 22:52:16 +0800

    Update DemoApplication.java
    ok

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-    // 土肥圆
+    // 土肥圆1
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
