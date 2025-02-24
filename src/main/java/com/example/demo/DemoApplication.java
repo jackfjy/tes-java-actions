018d55f90f7203be77afc5ec80c475a2d9c83df2
jackfjy <46479080+jackfjy@users.noreply.github.com>
2025-02-24 22:49:10 +0800

    Update DemoApplication.java
    测试workflow

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+    // 土肥圆
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
